9a494d86945c51bd106d182c7d3b7759ae35773b
John Otu <johnne01@gmail.com>
2020-09-29 18:55:54 +0100

    feat: connect database
    installed mongoose ODM and connected app to remote MongoDB instance
    
    fixes #3

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // App variables
 const app = express();
@@ -17,13 +18,20 @@ if (app.get("env") === "development") require("dotenv").config();
 
 const port = process.env.PORT || 4600;
 
+// Database connection
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'db connection error'));
+db.once('open', () => console.log('db connected'));
+
 // App configuration
 app.use(cors());
 app.use(helmet());
 
 // App routes
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Welcome to Team-023 project API service" })
+  res.status(200).json({ msg: "LoanDistro API service" })
 });
 
 // Server activation
